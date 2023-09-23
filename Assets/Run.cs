f07dca3489658e0bb29b7d229bf5a119006e2299
Alexander Lev <Sashalev200149@gmail.com>
2023-09-23 15:10:05 +0300

    Actor

--- Assets/Run.cs
+++ Assets/Run.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// public SpriteRenderer spriteRenderer;
 public class Run : MonoBehaviour {
     Animator anim;
     public float speed = 5f;
     public int state = 0;
     private Vector2 movement;
     private Rigidbody2D rb;
- 
+
+    public Sprite upSprite;
+    public Sprite downSprite;
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+    private SpriteRenderer spriteRenderer;
  
     // Use this for initialization
     void Start () {
         //anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = movement;
     }
@@ -49,35 +56,42 @@ public class Run : MonoBehaviour {
         //     }
         // }
  
-        // else
-        // {
+            //left/right/up/down
+            if (movement.x == -1)
+            {
+                ChangeObjectSprite(leftSprite);
+            }
  
-        //     //left/right/up/down
-        //     if (movement.x == -1)
-        //     {
-        //         anim.SetTrigger("move_left");
-        //     }
- 
-        //     if (movement.x == 1)
-        //     {
-        //         anim.SetTrigger("move_right");
-        //     }
+            if (movement.x == 1)
+            {
+               ChangeObjectSprite(rightSprite);
+            }
  
  
-        //     if (movement.y == 1)
-        //     {
-        //         anim.SetTrigger("move_up");
-        //     }
+            if (movement.y == 1)
+            {
+                ChangeObjectSprite(upSprite);
+            }
  
  
-        //     if (movement.y == -1)
-        //     {
-        //         anim.SetTrigger("move_down");
-        //     }
-        // }
+            if (movement.y == -1)
+            {
+                ChangeObjectSprite(downSprite);
+            }
  
- 
- 
+    }
+
+    void ChangeObjectSprite(Sprite newSprite)
+    {
+        if (spriteRenderer != null && newSprite != null)
+        {
+            // Устанавливаем новый спрайт для SpriteRenderer
+            spriteRenderer.sprite = newSprite;
+        }
+        else
+        {
+            Debug.LogError("SpriteRenderer или новый спрайт не установлены.");
+        }
     }
  
    
